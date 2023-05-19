69d582a5721972682718fdad63308abcbab78d6c
saqqdy <saqqdy@users.noreply.github.com>
2023-05-19 12:52:45 +0800

    release v2.0.0

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,18 +1,17 @@
 import { execSync } from 'child_process'
 import { version } from '../package.json'
-let [, , tagType] = process.argv
 
-if (tagType) tagType = tagType.replace(/^-+/, '')
+const [, , ...args] = process.argv
+const IS_TEST = args.includes('--test')
 
 const REGISTRY_URL = 'https://registry.npmjs.org'
 let command = `npm --registry=${REGISTRY_URL} publish --access public`
 
 if (version.includes('rc')) command += ' --tag release'
-if (version.includes('beta')) command += ' --tag beta'
-if (version.includes('alpha')) command += ' --tag alpha'
-
-if (tagType && !version.includes(tagType)) {
-	console.warn(`version ${version} is not a ${tagType} version`)
+else if (version.includes('beta')) command += ' --tag beta'
+else if (version.includes('alpha')) command += ' --tag alpha'
+else if (IS_TEST) {
+	console.warn(`${version} is not a test version, process exited`)
 	process.exit(0)
 }
 
